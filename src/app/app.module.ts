e50adac172b121054966fd86ca917c8c01c6ccba
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-10-09 14:45:26 +0100

    loading bar and http client module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -31,9 +33,11 @@ import {LoadingBarModule} from '@ngx-loading-bar/core'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     ToastrModule.forRoot(),
     LoadingBarRouterModule,
-    LoadingBarModule
+    LoadingBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
