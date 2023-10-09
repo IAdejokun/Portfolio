816363b3527a2f7fa3a107f3c5b44f35b20d8c8f
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-10-09 14:48:06 +0100

    blog feature added and blog component updated

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { BlogService } from 'src/app/services/blog.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent {
+
+  articleResult : any = [];
+
+  constructor(private blogService: BlogService, private toast:ToastrService){
+
+  }
+
+  ngOnInit():void{
+    this.blogData();
+  }
+
+  blogData(){
+    this.blogService.getUserArticles().subscribe({
+      next: (result) => {
+        this.articleResult = result;
+      },
+      error : (err) => {
+        this.toast.error('Try, Again', 'Error')
+      },
+    })
+  }
 
 }
