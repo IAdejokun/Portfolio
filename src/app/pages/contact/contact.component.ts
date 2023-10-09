2d1940a62cd3dcdc5406512c4f42dfa05ceec07d
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-10-09 18:21:29 +0100

    Contact component updated, form feature added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+
+  contactFormData = new FormGroup({
+
+    firstName:new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+
+    lastName:new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+
+    email:new FormControl('',[
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')
+    ]),
+
+    phoneNumber:new FormControl(''),
+
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
+  })
+
+  contactFormSubmit(){
+    console.log(this.contactFormData.value, '#userValue');
+  }
 
 }
