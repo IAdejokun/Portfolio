c6e75dacef693bdec9e9b469fc25c65117d8803b
Adejokun Ibukunoluwa <Adejokunibk@gmail.com>
2023-10-11 02:36:27 +0100

    contact component update, email js feature added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +10,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class ContactComponent {
 
+
+  constructor( private toast: ToastrService){}
 
   contactFormData = new FormGroup({
 
@@ -26,16 +30,46 @@ export class ContactComponent {
       Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')
     ]),
 
-    phoneNumber:new FormControl(''),
+    subject:new FormControl('',[
+      Validators.required,
+      Validators.minLength(3)
+    ]),
 
     message: new FormControl('', [
       Validators.required,
       Validators.minLength(3)
-    ])
+    ]),
+
+    to_name: new FormControl('Admin')
   })
 
-  contactFormSubmit(){
+  
+
+  async contactFormSubmit(){
+
     console.log(this.contactFormData.value, '#userValue');
+
+  try{
+    emailjs.init('3OAf2K82I_PfYov-q');
+    let response = await emailjs.send("service_okwc2hg","template_79li52i",{
+      lastName: this.contactFormData.value.lastName,
+      firstName: this.contactFormData.value.firstName,
+      to_name: this.contactFormData.value.to_name,
+      email: this.contactFormData.value.email,
+      subject: this.contactFormData.value.subject,
+      message: this.contactFormData.value.message,
+      });
+
+      this.toast.success('Email Sent', 'Success');
+      this.contactFormData.reset();
+    }
+    
+    catch(err){
+      this.toast.error('Email not Sent', 'Error!');
+      this.contactFormData.reset()
+    }
+
   }
 
 }
+
